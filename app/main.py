e9e5d13b759fa3ab80a149f7dc301f76e65210c3
Lujain Almulaifi <almulai@andrew.cmu.edu>
2025-11-09 20:57:19 +0000

    Initial commit

--- app/main.py
+++ app/main.py
@@ -28,9 +28,39 @@ def get_team_info(team_id: str):
     # TODO Fix this to return correct values for correct team ids.
     if team_id == "1":
         return {
-            "team_id" : team_id,
-            "team_name": "<name of team 1>",
+            "team_id" : 1,
+            "team_name": "nodegpt",
             "mentor": "Seckhen"
+        }
+    elif team_id == "2":
+        return {
+            "team_id" : 2,
+            "team_name": "bitwise",
+            "mentor": "Aadi"
+        }
+    elif team_id == "3":
+        return {
+            "team_id" : 3,
+            "team_name": "gituardo",
+            "mentor": "Steve"
+        }
+    elif team_id == "4":
+        return {
+            "team_id" : 4,
+            "team_name": "sick fault",
+            "mentor": "Seckhen"
+        }
+    elif team_id == "5":
+        return {
+            "team_id" : 5,
+            "team_name": "Debug Divas",
+            "mentor": "Aadi"
+        }
+    elif team_id == "6":
+        return {
+            "team_id" : 6,
+            "team_name": "Codex",
+            "mentor": "Steve"
         }
     else:
         raise HTTPException(status_code=404, detail="Invalid team id")
